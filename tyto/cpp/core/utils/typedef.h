db089559fc7c41597f2ffb544d4a2467ab97ddd0
chentaosheng <fixend@outlook.com>
2024-04-01 04:10:34 +0800

    定义基础数据类型

---
+++ tyto/cpp/core/utils/typedef.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+
+//
+// 基础类型定义
+
+// 有符号
+using int8 = std::int8_t;
+using int16 = std::int16_t;
+using int32 = std::int32_t;
+using int64 = std::int64_t;
+
+// 无符号
+using uint8 = std::uint8_t;
+using uint16 = std::uint16_t;
+using uint32 = std::uint32_t;
+using uint64 = std::uint64_t;
+
+// 浮点
+using float32 = float;
+using float64 = double;
