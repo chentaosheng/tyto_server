2cb11bf96dceb8c039c5c5aa4bc09d4c607ee9a8
chentaosheng <fixend@outlook.com>
2024-04-08 11:41:08 +0800

    添加单例模板

---
+++ tyto/csharp/core/Tyto.Utils/PatternUtil/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace Tyto.Utils.PatternUtil;
+
+using System;
+
+/// <summary>
+/// 单例模式，派生类必须将构造函数设置为protected或private
+/// </summary>
+public class Singleton<T> where T : class
+{
+	public static T Instance => SingletonCreator.s_instance;
+
+	protected Singleton()
+	{
+	}
+
+	private static class SingletonCreator
+	{
+		internal static readonly T s_instance = (T)Activator.CreateInstance(typeof(T), true)!;
+
+		static SingletonCreator()
+		{
+		}
+	}
+}
