223c05a69bfd130721f2b0fa4c188b54c4348f57
chentaosheng <fixend@outlook.com>
2024-04-11 02:43:12 +0800

    调整WaitQueue命名

--- tyto/go/core/syncutil/wait_queue.go
+++ tyto/go/core/syncutil/wait_queue.go
@@ -7,14 +7,14 @@ import (
 
 // 等待队列，会阻塞等待Pop操作
 type WaitQueue[T any] struct {
-	cond *sync.Cond
-	buff memutil.RingBuffer[T]
+	cond  *sync.Cond
+	queue memutil.RingBuffer[T]
 }
 
 func NewWaitQueue[T any](initCapacity int32) *WaitQueue[T] {
 	return &WaitQueue[T]{
-		cond: sync.NewCond(&SpinLock{}),
-		buff: *memutil.NewRingBuffer[T](initCapacity),
+		cond:  sync.NewCond(&SpinLock{}),
+		queue: *memutil.NewRingBuffer[T](initCapacity),
 	}
 }
 
@@ -22,43 +22,65 @@ func NewWaitQueue[T any](initCapacity int32) *WaitQueue[T] {
 func (q *WaitQueue[T]) Pop() T {
 	q.cond.L.Lock()
 
-	for q.buff.Empty() {
+	for q.queue.Empty() {
 		q.cond.Wait()
 	}
 
-	v, _ := q.buff.Pop()
+	v, _ := q.queue.Pop()
 
 	q.cond.L.Unlock()
 
 	return v
 }
 
+// 弹出最多len(vs)个元素
+func (q *WaitQueue[T]) PopSome(vs []T) int32 {
+	q.cond.L.Lock()
+
+	for q.queue.Empty() {
+		q.cond.Wait()
+	}
+
+	count := q.queue.PopSome(vs)
+
+	q.cond.L.Unlock()
+
+	return count
+}
+
 // 添加元素到队列尾部
 func (q *WaitQueue[T]) Push(v T) {
 	q.cond.L.Lock()
-	q.buff.Push(v)
-	q.cond.L.Unlock()
+	q.queue.Push(v)
 	q.cond.Signal()
+	q.cond.L.Unlock()
+}
+
+func (q *WaitQueue[T]) PushAll(vs []T) {
+	q.cond.L.Lock()
+	q.queue.PushAll(vs)
+	q.cond.Signal()
+	q.cond.L.Unlock()
 }
 
 // 返回队列长度
 func (q *WaitQueue[T]) Len() int32 {
 	q.cond.L.Lock()
 	defer q.cond.L.Unlock()
-	return q.buff.Len()
+	return q.queue.Len()
 }
 
 // 判断队列是否为空
 func (q *WaitQueue[T]) Empty() bool {
 	q.cond.L.Lock()
 	defer q.cond.L.Unlock()
-	return q.buff.Empty()
+	return q.queue.Empty()
 }
 
 // 清空队列
 func (q *WaitQueue[T]) Clear() {
 	q.cond.L.Lock()
-	q.buff.Clear()
-	q.cond.L.Unlock()
+	q.queue.Clear()
 	q.cond.Signal()
+	q.cond.L.Unlock()
 }
