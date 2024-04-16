83f0900a721e78470a7f931c062db4f7c87f1694
chentaosheng <fixend@outlook.com>
2024-04-17 05:05:00 +0800

    完善fileutil

--- tyto/go/core/fileutil/list.go
+++ tyto/go/core/fileutil/list.go
@@ -1,12 +1,11 @@
 package fileutil
 
 import (
-	"path"
 	"path/filepath"
 )
 
 // 列出指定目录下的文件
 func ListFile(dir string, globPattern string) ([]string, error) {
-	p := path.Join(dir, globPattern)
+	p := filepath.Join(dir, globPattern)
 	return filepath.Glob(p)
 }
--- tyto/go/core/fileutil/separator.go
+++ tyto/go/core/fileutil/separator.go
@@ -1,6 +1,7 @@
 package fileutil
 
 import (
+	"path/filepath"
 	"strings"
 )
 
@@ -12,4 +13,28 @@ func ToWindowsSeparator(path string) string {
 // 转换为Linux风格的路径分隔符
 func ToLinuxSeparator(path string) string {
 	return strings.ReplaceAll(path, "\\", "/")
+}
+
+// 移除路径末尾的分隔符
+func RemoveLastSeparator(path string) string {
+	if strings.HasSuffix(path, "/") || strings.HasSuffix(path, "\\") {
+		return path[:len(path)-1]
+	}
+
+	return path
+}
+
+// 添加路径末尾的分隔符
+func AppendLastSeparator(path string) string {
+	if strings.HasSuffix(path, "/") || strings.HasSuffix(path, "\\") {
+		return path
+	}
+
+	b := strings.Builder{}
+	b.Grow(len(path) + 1)
+
+	b.WriteString(path)
+	b.WriteByte(filepath.Separator)
+
+	return b.String()
 }
