dc00b4b3185fef30edd3cc81b79a916b0fae5497
chentaosheng <fixend@outlook.com>
2024-05-14 16:31:18 +0800

    调整命名

--- tyto/cpp/core/utils/spin_lock.cpp
+++ tyto/cpp/core/utils/spin_lock.cpp
@@ -10,10 +10,10 @@ namespace tyto
 	void SpinLock::Lock() noexcept
 	{
 		// 自旋方式等待的次数
-		constexpr int kPAUSE_COUNT_BEFORE_SLEEP = 32;
-		constexpr int kSLEEP_COUNT_BEFORE_YIELD = 64;
+		constexpr std::size_t kPauseCountBeforeSleep = 32;
+		constexpr std::size_t kSleepCountBeforeYield = 64;
 		// 计算退避时，用于计算退避次数的偏移值
-		constexpr std::size_t kMAX_BACKOFF_SHIFT = 16;
+		constexpr std::size_t kMaxBackOffShift = 16;
 
 		static thread_local std::minstd_rand generator{ std::random_device{}() };
 		// 获取锁时，与其他线程冲突的次数
@@ -25,12 +25,12 @@ namespace tyto
 			while (flag_.test(std::memory_order_relaxed))
 			{
 				// 按顺序用不同的方式自旋等待
-				if (retries < kPAUSE_COUNT_BEFORE_SLEEP)
+				if (retries < kPauseCountBeforeSleep)
 				{
 					++retries;
 					CpuRelax();
 				}
-				else if (retries < kSLEEP_COUNT_BEFORE_YIELD)
+				else if (retries < kSleepCountBeforeYield)
 				{
 					++retries;
 					static constexpr std::chrono::microseconds sleep_time{ 0 };
@@ -45,7 +45,7 @@ namespace tyto
 			if (flag_.test_and_set(std::memory_order_acquire))
 			{
 				// 获取失败，说明线程间竞争比较激烈，进行随机退避
-				std::size_t shift = (std::min)(conflict_count, kMAX_BACKOFF_SHIFT);
+				std::size_t shift = (std::min)(conflict_count, kMaxBackOffShift);
 				std::uniform_int_distribution<std::size_t> dist{
 					0,
 					static_cast<std::size_t>(1) << shift
